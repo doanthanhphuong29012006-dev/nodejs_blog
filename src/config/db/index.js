d00fb2aee1c2360ddd906d9617a247ba7ada4641
doanthanhphuong29012006-dev <doanthanhphuong29012006@gmail.com>
2026-03-01 03:03:46 +0700

    MONGODB_URI

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,12 +3,11 @@ mongoose.set('strictQuery', true);
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/phuongdt_dev', {
-        });
-        console.log('Successfully')
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/f8_education_dev');
+        
+        console.log('Connect successfully!!!');
     } catch (error) {
-        console.log('Failure')
-        console.log('Error: ', error.message)
+        console.log('Connect failure!!!');
     }
 }
 
